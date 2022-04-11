9eeee18e847768fb17dc5f2c88ee46f580c479bb
紀元翔 <corbin50111@gmail.com>
2022-04-11 11:19:51 +0800

    add lodingpage,loginpage 版面

--- refrigerator_label_front/src/App.js
+++ refrigerator_label_front/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+// import CheckboxG from './Components/CheckboxG';
+// import Load from './Pages/LoadingPage';
+import Login from './Pages/LoginPage';
 import './App.css';
 
 function App() {
   console.log(process.env.REACT_APP_URL)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login/>
     </div>
   );
 }
