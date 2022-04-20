567217614e87e62e08b126c1d06f5e958bea4635
unknown <garyopen1876@gmail.com>
2022-04-20 14:06:08 +0800

    install data grid

--- refrigerator_label_front/src/Components/CheckboxG.js
+++ refrigerator_label_front/src/Components/CheckboxG.js
@@ -1,320 +1,90 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
-import { visuallyHidden } from "@mui/utils";
-import TextField from "@mui/material/TextField";
-import BTNG from "./BtnG";
-import Btn from './SaveBtn'
+import React, {useState, useEffect} from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { makeStyles } from "@mui/styles";
+import axios from "../Axios.config.js"
 
-function createData(name, ID, Date) {
-  return {
-    name,
-    ID,
-    Date,
-  };
-}
-
-const rows = [
-  createData("Cupcake", 305, 3.7),
-  createData("Donut", 452, 25.0),
-  createData("Eclair", 262, 16.0),
-  createData("Frozen yoghurt", 159, 6.0),
-  createData("Gingerbread", 356, 16.0),
-  createData("Honeycomb", 408, 3.2),
-  createData("Ice cream sandwich", 237, 9.0),
-  createData("Jelly Bean", 375, 0.0),
-  createData("KitKat", 518, 26.0),
-  createData("Lollipop", 392, 0.2),
-  createData("Marshmallow", 318, 0),
-  createData("Nougat", 360, 19.0),
-  createData("Oreo", 437, 18.0),
-];
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
+//css
+const useStyles = makeStyles({
+  grid: {
+    display: "flex",
+    flexDirection: "column-reverse"
   }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
+});
 
-function getComparator(order, orderBy) {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  {
-    id: "name",
-    numeric: false,
-    disablePadding: true,
-    label: "物品所屬人",
-  },
-  {
-    id: "ID",
-    numeric: true,
-    disablePadding: false,
-    label: "ID",
-  },
-  {
-    id: "Date",
-    numeric: true,
-    disablePadding: false,
-    label: "放入日期",
-  },
-  {
-    id: "Remark",
-    numeric: true,
-    disablePadding: false,
-    label: "備註",
-  },
-  {
-    id: "Action",
-    numeric: true,
-    disablePadding: false,
-    label: <BTNG />,
-  },
-];
-
-function EnhancedTableHead(props) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all objects",
-            }}
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "center" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
+// 我為Menu功能，進行中文化，但我鎖住了，不用理
+const localizedTextsMap = { 
+  columnMenuUnsort: "原始排列",
+  columnMenuSortAsc: "升序排列",
+  columnMenuSortDesc: "降序排列",
+  columnMenuFilter: "篩選",
+  columnMenuHideColumn: "隱藏此列",
+  columnMenuShowColumns: "顯示此列"
 };
 
-export default function EnhancedTable() {
-  const [order, setOrder] = React.useState("asc");
-  const [orderBy, setOrderBy] = React.useState("ID");
-  const [selected, setSelected] = React.useState([]);
-  const [page] = React.useState(0);
-  const [dense] = React.useState(false);
-  const [rowsPerPage] = React.useState(15); //改一頁的資料量
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
+const columns = [
+  { field: 'owner', 
+    headerName: '物品所屬者', 
+    width: 150,
+    disableColumnMenu:true,
+    sortable: false,
+  },
+  {
+    field: 'date_id',
+    headerName: 'ID',
+    width: 150,
+    disableColumnMenu:true,
+    sortable: false,
+  },
+  {
+    field: 'date',
+    headerName: '放入日期',
+    type: 'date',
+    width: 150,
+    disableColumnMenu:true,
+  },
+  {
+    field: 'remark',
+    headerName: '備註',
+    width: 110,
+    disableColumnMenu:true,
+    sortable: false,
+  },
+];
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
+export default function DataGridDemo() {
 
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
+  const classes = useStyles();
 
-    setSelected(newSelected);
-  };
+  //data
+  const [rowData,setRowData] = useState([]);
+  useEffect(() => {
+    axios
+    .get("api/find_fridge_label_all")
+    .then((response) => {
+      const label_data = response['data']['message']
+      setRowData(response['data']['message']);
+      label_data.forEach(item => console.log(item))
+    })
+    .catch((error) => {
+      console.log(error)
+    });
 
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+  }, []);
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <Paper sx={{ width: "100%", mb: 2 }}>
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 400 }}
-            aria-labelledby="tableTitle"
-            size={dense ? "small" : "medium"}
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-                 rows.slice().sort(getComparator(order, orderBy)) */}
-              {stableSort(rows, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            "aria-labelledby": labelId,
-                          }}
-                        />
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.name}
-                      </TableCell>
-                      <TableCell
-                        align="center"
-                        component="th"
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.ID}
-                      </TableCell>
-                      <TableCell align="center">{row.Date}</TableCell>
-                      <TableCell
-                        align="right"
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        <TextField
-                          align="right"
-                          className="Remark"
-                          placeholder="編輯備註"
-                          defaultValue={row.Reamrk}
-                          size="small"
-                        />
-                      </TableCell>
-                      <TableCell
-                        align="center"
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        <Btn BtnText="儲存" Message = "編輯成功"/>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 23 : 43) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </Box>
+    <div style={{ height: 800, width: '100%' }}>
+      <DataGrid
+        className={classes.grid}
+        rows={rowData}
+        columns={columns}
+        pageSize={100}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+        localeText={localizedTextsMap}
+      />
+    </div>
   );
 }
--- refrigerator_label_front/src/Pages/ManagementPage.js
+++ refrigerator_label_front/src/Pages/ManagementPage.js
@@ -2,8 +2,8 @@ import Bar from "../Components/AppBar";
 import Checkbox from "../Components/CheckboxG";
 import "../App.css";
 
+
 function HomePage() {
-  console.log(process.env.REACT_APP_URL);
   return (
     <div className="Home">
       <div className="Bar">
