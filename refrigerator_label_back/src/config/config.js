e9eaf74ecd5c5c55a226d566df8b48d38171ace8
unknown <garyopen1876@gmail.com>
2022-04-30 02:21:06 +0800

    create seeder file

--- refrigerator_label_back/src/config/config.js
+++ refrigerator_label_back/src/config/config.js
@@ -1,28 +1,31 @@
 require("dotenv").config();
 module.exports = {
     "development": {
-        "username": process.env.DB_USER,
+        "username": process.env.DB_USERNAME,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
         "logging": process.env.DB_LOGGING,
         "dialect": process.env.DB_DIALECT,
         "dialectOptions": {"ssl": {"require": true,"rejectUnauthorized": false}},
     },
     "test": {
-        "username": process.env.DB_USER,
+        "username": process.env.DB_USERNAME,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
         "logging": process.env.DB_LOGGING,
         "dialect": process.env.DB_DIALECT,
         "dialectOptions": {"ssl": {"require": true,"rejectUnauthorized": false}} 
     },
     "production": {
-        "username": process.env.DB_USER,
+        "username": process.env.DB_USERNAME,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
         "logging": process.env.DB_LOGGING,
         "dialect": process.env.DB_DIALECT,
         "dialectOptions": {"ssl": {"require": true,"rejectUnauthorized": false}} 
