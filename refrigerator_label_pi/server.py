9d3f36bd6d9934cfc7a3ddc27492e4aee2dae731
unknown <garyopen1876@gmail.com>
2022-06-20 17:01:40 +0800

    update

--- refrigerator_label_pi/server.py
+++ refrigerator_label_pi/server.py
@@ -1,8 +1,7 @@
 import os
 import time
 import curses
-import requests
-import jwt
+import signal
 from dotenv import load_dotenv
 from PIL import Image
 from datetime import datetime, timezone, timedelta
@@ -10,22 +9,30 @@ from brother_ql.conversion import convert
 from brother_ql.backends.helpers import send
 from brother_ql.raster import BrotherQLRaster
 from pngMaker import pngMake
-# load env
+from apiCaller import delete_label_api, new_label_api, change_printer_state_api
+
+# Load .env
 load_dotenv()
 
-os.system("sudo chmod 666 /dev/usb/lp0")
+# State
+printer_state = False
 
-# time setting
+# QL Setting
+backend = "linux_kernel"    # 'pyusb', 'linux_kernel', 'network'
+# Get these values from the Windows usb driver filter.  Linux/Raspberry Pi uses '/dev/usb/lp0'.
+printer = "/dev/usb/lp0"
+printer_model = "QL-700"  # your printer model.
+
+# timeout
+
+
+def handle_timeout(signum, frame):
+    raise TimeoutError('function timeout')
+
+
+# Time Setting
 tz = timezone(timedelta(hours=+8))
 dt = datetime.now(tz)
-
-# label printer check
-while(True):
-    if os.system("sudo chmod 666 /dev/usb/lp0") == 0:
-        break
-    else:
-        print("please insert the label printer")
-        time.sleep(5)
 
 # curses wait(code wait)
 screen = curses.initscr()
@@ -39,33 +46,42 @@ def stop(curse):
 
 
 while True:
+    # data clear check
+    data_clear = False
+    continue_check = False
+
+    while True:
+        result_code = os.system("sudo chmod 666 {}".format(printer))
+        if result_code == 0:
+            if not printer_state:
+                printer_state = True
+                change_printer_state_api("success")
+            break
+        else:
+            print("please insert the label printer")
+            if printer_state:
+                printer_state = False
+                change_printer_state_api("error")
+            time.sleep(5)
+
     # curses wait(code wait)
     curses.wrapper(stop)
     curses.curs_set(1)
+
+    # label printer ready
     input_data = input("input data:")
 
     if input_data[:6] == 'label:':
         print("delete label start!")
         # http delete
-        response = requests.delete(
-            "{}{}".format(os.getenv("SERVER_URL"), "api/label"),
-            headers={'token': jwt.encode(
-                {"IoT": "delete"}, os.getenv("JWT_SECRET"), algorithm="HS256")},
-            data={"labelId": input_data.replace("label:", "")}
-        )
-        print(response.json())
+        delete_label_api(input_data)
     else:
         print("print label start!")
         # now date
         date_now = dt.strftime("%Y-%m-%d %H:%M:%S")
 
         # http post
-        response = requests.post(
-            "{}{}".format(os.getenv("SERVER_URL"), "api/label"),
-            headers={'token': jwt.encode(
-                {"IoT": "print"}, os.getenv("JWT_SECRET"), algorithm="HS256")},
-            data={"date": date_now, "cardId": input_data}
-        )
+        response = new_label_api(date_now, input_data)
 
         if(response.status_code == 201):  # is member
             # make png
@@ -73,20 +89,13 @@ while True:
             pngMake(member_name=response_json["name"],
                     label_id=response_json["labelId"], date=date_now.split(' ')[0])
 
-            # QL print
-            label_img = Image.open("label.png")
-            backend = "linux_kernel"    # 'pyusb', 'linux_kernel', 'network'
-            model = "QL-700"  # your printer model.
-            # Get these values from the Windows usb driver filter.  Linux/Raspberry Pi uses '/dev/usb/lp0'.
-            printer = "/dev/usb/lp0"
-
-            qlr = BrotherQLRaster(model)
+            qlr = BrotherQLRaster(printer_model)
             qlr.exception_on_warning = True
 
             instructions = convert(
                 qlr=qlr,
                 # Takes a list of file names or PIL objects.
-                images=[label_img],
+                images=[Image.open("label.png")],
                 label="39x90",
                 rotate="90",    # 'Auto', '0', '90', '270'
                 threshold=70.0,    # Black and white threshold in percent.
@@ -98,21 +107,46 @@ while True:
                 cut=True
             )
 
-            try:
-                action = send(instructions=instructions, printer_identifier=printer,
-                              backend_identifier=backend, blocking=True)
-                if not action['printer_state']['errors']:
-                    print("print label successful")
-                else:
-                    # http post
-                    response = requests.delete(
-                        "{}{}".format(os.getenv("SERVER_URL"), "api/label"),
-                        headers={"token": jwt.encode(
-                            {"IoT": "print"}, os.getenv("JWT_SECRET"), algorithm="HS256")},
-                        data={"labelId": response_json["labelId"]}
-                    )
-            except PermissionError:
-                print("please checkout you have insert the label printer")
-                os.system("sudo chmod 666 /dev/usb/lp0")
+            while True:
+                signal.signal(signal.SIGALRM, handle_timeout)
+                signal.alarm(10)
+                try:
+                    action = send(instructions=instructions, printer_identifier=printer,
+                                  backend_identifier=backend, blocking=True)
+                    if not action['printer_state']['errors']:
+                        print("print label successful")
+                    else:
+                        data_clear = True
+                except FileNotFoundError:
+                    print("No printer! Please insert the label printer!")
+                    data_clear = True
+                except TimeoutError:
+                    print("Please close your printer!")
+                    if printer_state:
+                        printer_state = False
+                        change_printer_state_api("error")
+                    continue
+                except PermissionError:
+                    os.system("sudo chmod 666 {}".format(printer))
+                    continue
+                except TypeError:
+                    print("System Error")
+                    data_clear = True
+                except OSError:
+                    print("Printer printing! Don't pull it!")
+                    data_clear = True
+                finally:
+                    signal.alarm(0)
+                break
+
+            # error data clear
+            if data_clear:
+                # http delete
+                delete_label_api(response_json["labelId"])
+                # http post state change
+                if printer_state:
+                    printer_state = False
+                    change_printer_state_api("error")
+
         else:  # not member
             print(response.json())
