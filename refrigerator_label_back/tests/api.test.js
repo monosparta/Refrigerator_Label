6442b2aa83c3c7243031b3360423e57f8bf6e346
unknown <garyopen1876@gmail.com>
2022-06-28 13:27:32 +0800

    update test & controller admin fix

--- refrigerator_label_back/tests/api.test.js
+++ refrigerator_label_back/tests/api.test.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
-const request = require("supertest");
-const jwt = require("jsonwebtoken");
 const express = require("express");
 const router = express.Router();
 require("../src/routes/index.js")(router);
 const app = express();
 app.use(express.json());
 app.use("/", router);
+
+const request = require("supertest");
+
+const jwt = require("jsonwebtoken");
+const test_token = jwt.sign({ super: "test" }, process.env.JWT_SECRET);
 
 test("Backend Start Test", (done) => {
   request(app)
@@ -23,7 +26,7 @@ describe("User Test", () => {
     request(app)
       .get("/api/find_user_all")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .expect("Content-Type", /json/)
       .expect(200)
       .end(function (err, res) {
@@ -39,7 +42,7 @@ describe("Label Test", () => {
     request(app)
       .get("/api/find_label_all")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .expect("Content-Type", /json/)
       .expect(200)
       .end(function (err, res) {
@@ -52,7 +55,7 @@ describe("Label Test", () => {
     request(app)
       .post("/api/label")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .send({ date: "2022-06-27" })
       .expect("Content-Type", /json/)
       .expect(403)
@@ -66,7 +69,7 @@ describe("Label Test", () => {
     request(app)
       .post("/api/label")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .send({ cardId: "1234567890" })
       .expect("Content-Type", /json/)
       .expect(403)
@@ -80,7 +83,7 @@ describe("Label Test", () => {
     request(app)
       .post("/api/label")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .send({ date: "2022-06-27", cardId: "0000000000" })
       .expect("Content-Type", /json/)
       .expect(403)
@@ -94,7 +97,7 @@ describe("Label Test", () => {
     request(app)
       .post("/api/label")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .send({ date: "2022-06-28", cardId: "1745718229" })
       .expect("Content-Type", /json/)
       .expect(201)
@@ -109,7 +112,7 @@ describe("Label Test", () => {
     request(app)
       .delete("/api/label")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .send({})
       .expect("Content-Type", /json/)
       .expect(403)
@@ -123,7 +126,7 @@ describe("Label Test", () => {
     request(app)
       .delete("/api/label")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
       .send({ labelId: label_id })
       .expect("Content-Type", /json/)
       .expect(200)
@@ -135,11 +138,156 @@ describe("Label Test", () => {
 });
 
 describe("Admin Test", () => {
-  test("Get admin", (done) => {
+  test("Get Admin", (done) => {
     request(app)
       .get("/api/admin")
       .set("Accept", "application/json")
-      .set("token", jwt.sign({ super: "test" }, process.env.JWT_SECRET))
+      .set("token", test_token)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Create Admin Data No Match A", (done) => {
+    request(app)
+      .post("/api/admin")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({
+        password: "test_root",
+        mail: "test_root@mail.com",
+      })
+      .expect("Content-Type", /json/)
+      .expect(402)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Create Admin Data No Match B", (done) => {
+    request(app)
+      .post("/api/admin")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({
+        username: "test_root",
+        mail: "test_root@mail.com",
+      })
+      .expect("Content-Type", /json/)
+      .expect(402)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Create Admin Data No Match C", (done) => {
+    request(app)
+      .post("/api/admin")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({
+        username: "test_root",
+        password: "test_root",
+      })
+      .expect("Content-Type", /json/)
+      .expect(402)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Create Admin Success", (done) => {
+    request(app)
+      .post("/api/admin")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({
+        username: "test_root",
+        password: "test_root",
+        mail: "test_root@mail.com",
+      })
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Create Admin Again", (done) => {
+    request(app)
+      .post("/api/admin")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({
+        username: "test_root",
+        password: "test_root",
+        mail: "test_root@mail.com",
+      })
+      .expect("Content-Type", /json/)
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Reset Password Data No Match", (done) => {
+    request(app)
+      .put("/api/reset_password")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({})
+      .expect("Content-Type", /json/)
+      .expect(402)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Reset Password Not Self", (done) => {
+    request(app)
+      .put("/api/reset_password")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({ username: "test_root" })
+      .expect("Content-Type", /json/)
+      .expect(401)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Delete Admin Data No Match", (done) => {
+    request(app)
+      .delete("/api/admin")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({})
+      .expect("Content-Type", /json/)
+      .expect(402)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Delete Admin Success", (done) => {
+    request(app)
+      .delete("/api/admin")
+      .set("Accept", "application/json")
+      .set("token", test_token)
+      .send({
+        username: "test_root",
+      })
       .expect("Content-Type", /json/)
       .expect(200)
       .end(function (err, res) {
