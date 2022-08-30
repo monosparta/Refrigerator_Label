74005ff674b5c2ca754f0b4245393f1d3d862e67
unknown <garyopen1876@gmail.com>
2022-08-30 16:28:45 +0800

    add update user

--- refrigerator_label_back/src/controller/user.js
+++ refrigerator_label_back/src/controller/user.js
@@ -3,7 +3,7 @@ const user_service = require("../services/user.js");
 const user_update = async (_req, res) => {
   try {
     await user_service.user_update();
-    return res.status(200).json({ message: "update successfully" });
+    return res.status(200).json({ message: "Update successfully" });
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
